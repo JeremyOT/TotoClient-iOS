9cfd5e0283276af21f70ec2a43835e7857a02a19
Jeremy Olmsted-Thompson <jeremyot@gmail.com>
2015-04-23 03:39:51 -0400

    add framework build target

---
+++ TotoClient/TotoClient.h
@@ -0,0 +1,23 @@
+//
+//  TotoClient.h
+//  TotoClient
+//
+//  Created by Jeremy Olmsted-Thompson on 4/23/15.
+//  Copyright (c) 2015 JOT. All rights reserved.
+//
+
+#ifndef TotoClient_TotoClient_h
+#define TotoClient_TotoClient_h
+
+#import "TCDataService.h"
+#import "TCDataCache.h"
+#import "TCAsyncImageView.h"
+#import "TCAsyncImageView_Protected.h"
+#import "NSString+TCHash.h"
+#import "TCDelayedDispatcher.h"
+#import "TCHMAC.h"
+#import "TCUUID.h"
+#import "NSURL+TCQuery.h"
+#import "TotoService.h"
+
+#endif
---
+++ TotoClientFramework/TotoClientFramework.h
@@ -0,0 +1,28 @@
+//
+//  TotoClientFramework.h
+//  TotoClientFramework
+//
+//  Created by Jeremy Olmsted-Thompson on 4/23/15.
+//  Copyright (c) 2015 JOT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TotoClientFramework.
+FOUNDATION_EXPORT double TotoClientFrameworkVersionNumber;
+
+//! Project version string for TotoClientFramework.
+FOUNDATION_EXPORT const unsigned char TotoClientFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TotoClientFramework/PublicHeader.h>
+#import <TotoClientFramework/TCDataService.h>
+#import <TotoClientFramework/TCDataCache.h>
+#import <TotoClientFramework/TCAsyncImageView.h>
+#import <TotoClientFramework/TCAsyncImageView_Protected.h>
+#import <TotoClientFramework/NSString+TCHash.h>
+#import <TotoClientFramework/TCDelayedDispatcher.h>
+#import <TotoClientFramework/TCHMAC.h>
+#import <TotoClientFramework/TCUUID.h>
+#import <TotoClientFramework/NSURL+TCQuery.h>
+#import <TotoClientFramework/TotoService.h>
+
